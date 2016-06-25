29a22f00f62acb399ba884183fd4a3ff939ed5af
John Kluck <johnkluck5@gmail.com>
2016-06-25 11:52:52 -0400

    second commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,8 +38,9 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5</div>
+                <div class="title">ded</div>
             </div>
         </div>
     </body>
 </html>
+ahhhhhhhhhhhhhhhhh
